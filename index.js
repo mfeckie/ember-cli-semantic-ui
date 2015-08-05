2d2c7d84a70e493e2e265de118f1d6b56a066517
Martin Feckie <mfeckie@gmail.com>
2015-08-05 15:43:28 +1000

    Initial import of semantic-ui

--- index.js
+++ index.js
@@ -2,5 +2,23 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-semantic-ui'
+  name: 'ember-cli-semantic-ui',
+  included: function (app) {
+    app.import({
+      development: 'bower_components/semantic-ui/dist/semantic.css',
+      production: 'bower_components/semantic-ui/dist/semantic.min.css'
+    })
+
+    app.import({
+      development: 'bower_components/semantic-ui/dist/semantic.js',
+      production: 'bower_components/semantic-ui/dist/semantic.min.js'
+    })
+
+    var extensions = ['eot','otf','svg','ttf','woff','woff2'];
+    extensions.forEach(function(ext) {
+      app.import(`bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.${ext}`, {
+        destDir: 'assets/themes/default/assets/fonts'
+      });
+    });
+  }
 };
