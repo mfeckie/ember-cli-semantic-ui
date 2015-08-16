031d0dc6a44b949029a8d37ebe53722040961774
Martin Feckie <mfeckie@gmail.com>
2015-08-16 10:11:45 +1000

    Switch to using Semantic-UI JS for accordion

--- addon/components/ui-accordion-section.js
+++ addon/components/ui-accordion-section.js
@@ -2,12 +2,5 @@ import Ember from 'ember';
 import layout from '../templates/components/ui-accordion-section';
 
 export default Ember.Component.extend({
-  layout: layout,
-  action: 'accordionToggle',
-  targetObject: Ember.computed.alias('parentView'),
-  actions: {
-    accordionToggle () {
-      this.sendAction('action',this.$('.title, .content'));
-    }
-  }
+  layout: layout
 });
--- addon/components/ui-accordion.js
+++ addon/components/ui-accordion.js
@@ -2,14 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['ui', 'accordion'],
-  action: 'accordionToggle',
-  actions: {
-    accordionToggle: function (elements) {
-      if(elements.hasClass('active')) {
-        return elements.removeClass('active');
-      }
-      this.$('.title, .content').removeClass('active');
-      elements.addClass('active');
-    }
+  didInsertElement: function () {
+    this._super();
+    this.$().accordion();
   }
 });
