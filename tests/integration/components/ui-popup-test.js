ce95cc471f3e5e6898394782d058bd1cc2ffc3a9
Martin Feckie <mfeckie@gmail.com>
2015-08-23 08:35:57 +0800

    Add tests for hover on popup

--- tests/integration/components/ui-popup-test.js
+++ tests/integration/components/ui-popup-test.js
@@ -1,5 +1,7 @@
+/* globals QUnit */
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('ui-popup', 'Integration | Component | ui popup', {
   integration: true
@@ -14,4 +16,19 @@ test('it renders', function(assert) {
   `);
   // TODO - Need to find a way to trigger and test the hover event
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('It displays popup on hover', function (assert){
+  this.render(hbs`
+    {{#ui-popup content='Test popup'}}
+      template block text
+    {{/ui-popup}}
+  `);
+  QUnit.stop();
+  this.$('span').triggerHandler('mouseover');
+  Ember.run.later(() => {
+    QUnit.start();
+    var popup = Ember.$('.ui.popup.top.left.transition.visible');
+    assert.ok(popup.length === 1);
+  }, 300);
 });
