02d65764f298cd6e27100b87a432c9903ef15ec2
ChrizzDF <tools.christoph.langhof@gmail.com>
2015-08-23 21:20:16 +0200

    Fixed UNEXPECTED TOKEN error
    This error occurred after executing ember server in terminal.

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ module.exports = {
     app.import({
       development: 'bower_components/semantic-ui/dist/semantic.css',
       production: 'bower_components/semantic-ui/dist/semantic.min.css'
-    })
+    });
 
     app.import({
       development: 'bower_components/semantic-ui/dist/semantic.js',
       production: 'bower_components/semantic-ui/dist/semantic.min.js'
-    })
+    });
 
     var extensions = ['eot','otf','svg','ttf','woff','woff2'];
     extensions.forEach(function(ext) {
-      app.import(`bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.${ext}`, {
+      app.import('bower_components/semantic-ui/dist/themes/default/assets/fonts/icons.${ext}', {
         destDir: 'assets/themes/default/assets/fonts'
       });
     });
